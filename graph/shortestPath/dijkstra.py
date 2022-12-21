0d8ac56d727e57be42f42ad61a46b0b1850f0e2d
nayounsang <nayounsang722@gmail.com>
2022-12-21 13:31:18 +0900

    최단경로역추적

--- graph/shortestPath/dijkstra.py
+++ graph/shortestPath/dijkstra.py
@@ -1,3 +1,4 @@
+from collections import deque
 from heapq import heappush, heappop
 
 
@@ -11,3 +12,18 @@ def dijkstra(graph, distance, start):
             if new < distance[n]:  # if improve
                 heappush(heap, (new, n))
                 distance[n] = new  # update shorter path
+
+
+def chase_path(rvs, distance, s, e):
+    # if undirected graph: u can just input raw graph
+    # req hardcoding if path have its standards
+    result = [e]
+    cur = e
+    while cur != s:
+        for n,d in rvs[cur]:
+            diff = distance[cur] - distance[n]
+            if diff == d:
+                cur = n
+                result.append(n)
+    return result[::-1]
+
